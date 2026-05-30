81cfbc9bf685955ab91820d691729dbde219ee05
AlexanderBlackmore <129782333+AlexanderBlackmore@users.noreply.github.com>
2026-05-29 17:10:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/', function(req, res){
-    res.render('home'); 
+    res.render('test'); 
     });
 
 const path = require("path");
@@ -38,39 +38,16 @@ const handleError = (err, res) => {
     .end("Oops! Something went wrong!");
 };
 
-const upload = multer({
-  dest: "/public/images"
-});
-
-app.post('/upload', upload.single('file'), function(req, res) {
-  var file = './public/images' + '/' + req.file.originalname;
-  fs.rename(req.file.path, file , function(err) {
-      res.redirect('/')
-  });
-});
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.get('/api/images', (req, res) => {
-    fs.readdir('./public/images', (err, files) => {
-        const images = files.filter(file => /\.(jpe?g|png|gif|webp)$/i.test(file));
-        res.json(images);
-    });
-});
-
 app.get('/read-file', (req, res) => {
-    // Asynchronously read the text file
     fs.readFile('example.txt', 'utf8', (err, data) => {
         if (err) {
             return res.status(500).send('Error reading file');
         }
-        res.send(data); // Send file content back to browser
+        res.send(data); 
     });
 });
 
 app.post('/write-to-file', (req, res) => {
-
-    // fs.appendFile adds to the file; fs.writeFile overwrites it
     fs.writeFile('example.txt', 'verify', (err) => {
         if (err) {
             console.error(err);
